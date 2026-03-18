5c1a165fe957a4c962d7a92b988c3c2d988c4a48
Juan1747 <juchaconm@unal.edu.co>
2026-03-18 12:33:06 -0500

    database.py modified

--- src/core/database.py
+++ src/core/database.py
@@ -3,15 +3,14 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from src.core.config import settings
 
 # 1. El Motor (Engine): Es el puente de comunicación. 
-# Toma la URL de tu .env (localhost:5432) y traduce las consultas de Python a PostgreSQL.
+# Toma la URL del .env  y traduce las consultas de Python a PostgreSQL.
 engine = create_engine(settings.DATABASE_URL)
 
 # 2. La Fábrica de Sesiones: Cada vez que un usuario haga una petición (ej. registrarse),
 # esto creará una conexión fresca y aislada hacia la base de datos.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# 3. La Base Maestra: De esta clase van a heredar TODAS las tablas de tu sistema.
-# Es lo que permite centralizar las tablas de 'users', 'auth' y 'preferences' en un solo lugar.
+# 3. Es lo que permite centralizar las tablas de 'users', 'auth' y 'preferences' en un solo lugar.
 Base = declarative_base()
 
 # 4. Inyección de Dependencias para FastAPI
